4e6c06b4721da87c30f8737fa029031368e3733e
Rafael Ehlers <rafaehlers@gmail.com>
2013-09-24 05:39:42 -0300

    adicionado <title> e contadores nos números

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+(function($){
+    $(document).ready(function(){
 
+    jQuery('.milestone-counter').appear(function() {
+        $('.milestone-counter').each(function(){
+            dataperc = $(this).attr('data-perc'),
+            $(this).find('.milestone-count').delay(6000).countTo({
+                from: 0,
+                to: dataperc,
+                speed: 2000,
+                refreshInterval: 100
+            });
+        });
+    });
+
+    });
+})(window.jQuery);
