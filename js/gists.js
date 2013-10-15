c272607cf8b68293c9f1a0e3382324393dfb1ad6
Rafael Ehlers <rafaehlers@gmail.com>
2013-10-15 18:13:23 -0300

    Testando Gists

---
+++ js/gists.js
@@ -0,0 +1,31 @@
+(function($){
+    $(document).ready(function(){
+
+        var ghusername = 'guwordpressbrasil';
+
+        function getgists(githubuser) {
+            $.getJSON("http://gist.github.com/api/v1/json/gists/" +githubuser+ "?callback=?", function(data) {
+                var gisthtml = '';
+                for (var i = 0; i < 10;) {
+                    var item = data.gists[i];
+                    if (item.public) {
+                        i++;
+                        var desc = item.description;
+                        if (desc === null) {
+                            desc = '(no description)';
+                        }
+                        else {if (desc.length > 50) {
+                            desc = desc.substring(0, 40).replace(/w+$/, '') + '&#133;';
+                        }}
+                        gisthtml += '<li class="gist"><a href="http://gist.github.com/' + item.repo + '"><span class="gist-id">#' + item.repo + ': </span>' + desc + '</a></p>';
+                    }
+                };
+                if (data.gists.length > 10) {gisthtml += '<li class="gist gist-more"><a href="http://gist.github.com/' + githubuser + '">More snippets&#133;</a>'};
+               $("#gists").html('<ul class="gist-list">'+gisthtml+'</ul>');
+            });
+        }
+
+        getgists(ghusername);
+
+    });
+})(window.jQuery);
